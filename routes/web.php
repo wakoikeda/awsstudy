67904d72a7a9fbe8e057c8801ae599b21136fe8c
wakoikeda <wakowako1009@icloud.com>
2024-05-21 10:44:17 +0000

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,15 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
-
-
-Route::get('/posts', [PostController::class, 'index']);   /*
+use App\Http\Controllers\PostController;      //追加
+/*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index']);
+?>
