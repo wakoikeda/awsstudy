de8b29a94d8bb24fb4006d545073c782431e83ab
wakoikeda <wakowako1009@icloud.com>
2024-05-29 07:40:03 +0000

    ブログ詳細表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,10 +7,21 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(Post $post)
-    {
-        return view('posts.index')->with(['posts' => $post->get()]);  
-       //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
-    }
+   public function index(Post $post)
+{
+    return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
 }
+/**
+ * 特定IDのpostを表示する
+ *
+ * @params Object Post // 引数の$postはid=1のPostインスタンス
+ * @return Reposnse post view
+ */
+public function show(Post $post)
+{
+    return view('posts.show')->with(['post' => $post]);
+ //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+}
+}
+
 ?>
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;      //追加
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
+// '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
 ?>
